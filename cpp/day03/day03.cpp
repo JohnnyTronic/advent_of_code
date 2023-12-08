9563f0d8f5cf9614609f0e720e78379dbf358280
John <3317344+JohnnyTronic@users.noreply.github.com>
2023-12-08 11:55:24 -0500

    Shifting Day03 to a bounding-box technique.

--- cpp/day03/day03.cpp
+++ cpp/day03/day03.cpp
@@ -3,104 +3,146 @@
 #include <sstream>
 #include <vector>
 #include <set>
+#include <regex>
+#include <array>
 
-class Coord2D {
+class Vec2 {
 public:
     int x = 0;
     int y = 0;
-    int gridWidth;
-    int gridHeight;
 
-    Coord2D(int x, int y, int gridWidth, int gridHeight) : x(x), y(y), gridWidth(gridWidth), gridHeight(gridHeight) {};
+    Vec2() {};
+    Vec2(int x, int y) : x(x), y(y) {}
 
-    bool isOnGrid() {
-        return x >= 0 && x < gridWidth && y >= 0 && y < gridHeight;
-    }
-
-    int ToCoord1D() {
-        return y * gridWidth + x;
+    friend Vec2 operator+(const Vec2 left, const Vec2 right) {
+        return Vec2(left.x + right.x, left.y + right.y);
     }
 };
 
-int Coord2Dto1D(Coord2D coord, int gridWidth, int gridHeight) {
-    return coord.y * gridWidth + coord.x;
-}
-
-Coord2D Coord1Dto2D(int index, int gridWidth, int gridHeight) {
-    Coord2D coord(index % gridWidth, index / gridWidth, gridWidth, gridHeight);   
-    return coord;
-}
-
-std::vector<int> GenerateNeighbourIndicies(int index, int gridWidth, int gridHeight) {
+template <typename T>
+class Grid2D {
+public:    
     
-    Coord2D targetCell = Coord1Dto2D(index, gridWidth, gridHeight);
+    std::vector<std::vector<T>> content;
 
-    std::vector<int> neighbourIndicies;
+    int getWidth() {
+        return this->content.size() == 0 ? 0 : this->content[0].size() == 0 ? 0 : this->content[0].size();
+    }
 
-    Coord2D topLeft = Coord2D(targetCell.x - 1, targetCell.y - 1, gridWidth, gridHeight);
-    if (topLeft.isOnGrid())
-        neighbourIndicies.push_back(topLeft.ToCoord1D());
-
-    Coord2D top = Coord2D(targetCell.x, targetCell.y - 1, gridWidth, gridHeight);
-    if (top.isOnGrid())
-        neighbourIndicies.push_back(top.ToCoord1D());
-
-    Coord2D topRight = Coord2D(targetCell.x + 1, targetCell.y - 1, gridWidth, gridHeight);
-    if (topRight.isOnGrid())
-        neighbourIndicies.push_back(topRight.ToCoord1D());
-
-    Coord2D left = Coord2D(targetCell.x - 1, targetCell.y, gridWidth, gridHeight);
-    if (left.isOnGrid())
-        neighbourIndicies.push_back(left.ToCoord1D());
-
-    Coord2D right = Coord2D(targetCell.x + 1, targetCell.y, gridWidth, gridHeight);
-    if (right.isOnGrid())
-        neighbourIndicies.push_back(right.ToCoord1D());
-
-    Coord2D bottomLeft = Coord2D(targetCell.x - 1, targetCell.y + 1, gridWidth, gridHeight);
-    if (bottomLeft.isOnGrid())
-        neighbourIndicies.push_back(bottomLeft.ToCoord1D());
-
-    Coord2D bottom = Coord2D(targetCell.x, targetCell.y + 1, gridWidth, gridHeight);
-    if (bottom.isOnGrid())
-        neighbourIndicies.push_back(bottom.ToCoord1D());
-
-    Coord2D bottomRight = Coord2D(targetCell.x + 1, targetCell.y + 1, gridWidth, gridHeight);
-    if (bottomRight.isOnGrid())
-        neighbourIndicies.push_back(bottomRight.ToCoord1D());
+    int getHeight() {
+        return this->content.size();
+    }
     
-    return neighbourIndicies;
-}
+    bool isOnGrid(Vec2& position) {
+        return position.x >= 0 && position.x < this->getWidth() && position.y >= 0 && position.y < this->getHeight();
+    }
+    
+    std::vector<Vec2> GenerateNeighbourPositions(const Vec2& position) {
+
+        std::vector<Vec2> possibleNeighbourPositions;
+
+        possibleNeighbourPositions.push_back(position + Vec2(-1, -1));
+        possibleNeighbourPositions.push_back(position + Vec2(0, -1));
+        possibleNeighbourPositions.push_back(position + Vec2(1, -1));
+        possibleNeighbourPositions.push_back(position + Vec2(-1, 0));
+        possibleNeighbourPositions.push_back(position + Vec2(1, 0));
+        possibleNeighbourPositions.push_back(position + Vec2(-1, 1));
+        possibleNeighbourPositions.push_back(position + Vec2(0, 1));
+        possibleNeighbourPositions.push_back(position + Vec2(1, 1));
+
+        std::vector<Vec2> onGridNeighbours;
+        for (auto& possibleNeighbour : possibleNeighbourPositions) {
+            if(this.isOnGrid(possibleNeighbour)) {
+                onGridNeighbours.push_back(possibleNeighbour);
+            }
+        }
+
+        return onGridNeighbours;
+    }
+};
+
+class PartNumber {
+public:
+    int value;
+    Vec2 gridPosition;
+    int length;
+
+    PartNumber(int value, Vec2 gridPosition, int length) : value(value), gridPosition(gridPosition), length(length) {}
+};
+
+class Symbol {
+public:
+    std::string value;
+    Vec2 gridPosition;
+
+    Symbol(std::string value, Vec2 gridPosition) : value(value), gridPosition(gridPosition) {}
+
+    friend std::ostream& operator<< (std::ostream& out, const Symbol& symbol) {
+        out << "Symbol[\'" << symbol.value << ", (" << symbol.gridPosition.x << "," << symbol.gridPosition.y << ")";
+    }
+};
 
 int main()
 {
     std::cout << "Advent of Code 2023 - Day 3\n";
 
+    std::vector<PartNumber> partNumbers;
     int sumOfPartNumbers = 0;
 
     std::ifstream ifs("test_input.txt");
     //std::ifstream ifs("input.txt");
-    std::string fileContent;
     std::string line;    
 
-    int gridWidth = 0;
-    int gridHeight = 0;
+    Grid2D<char> inputGrid;
+    Grid2D<bool> dirtyGrid;
+    std::regex partNumberPattern("[0-9]+");
+    std::regex symbolPattern("[^0-9.]+");
+    int rowIndex = 0;
     while (std::getline(ifs, line)) {
-        fileContent += line;
-        gridWidth = line.length();
-        gridHeight++;
-    }
+        // Search for Part Numbers
+        auto partNumberMatchesBegin = std::sregex_iterator(line.begin(), line.end(), partNumberPattern);
+        std::sregex_iterator partNumberMatchesEnd;
+        for (auto iter = partNumberMatchesBegin; iter != partNumberMatchesEnd; iter++) {
+            std::smatch partNumberMatch = *iter;
+            std::cout << "Found part number: " << partNumberMatch.str() << std::endl;
+            auto partNumberString = partNumberMatch.str();
+            auto partNumberValue = std::stoi(partNumberString);
+            PartNumber newPartNumber(partNumberValue, Vec2(partNumberMatch.position(), rowIndex), partNumberString.length());
+            partNumbers.push_back(newPartNumber);
+        }
 
-    std::set<int> visitedCells;
+        // Search for Symbols
+        auto symbolMatchesBegin = std::sregex_iterator(line.begin(), line.end(), symbolPattern);
+        std::sregex_iterator symbolMatchesEnd;
+        for (auto iter = symbolMatchesBegin; iter != symbolMatchesEnd; iter++) {
+            std::smatch symbolMatch = *iter;
+            std::cout << "Found symbol: " << symbolMatch.str() << std::endl;
+            auto symbolString = symbolMatch.str();
+            Symbol newSymbol(symbolString, Vec2(symbolMatch.position(), rowIndex));
+        }
+
+        std::vector<char> newRow;
+        std::vector<bool> dirtyRow;
+        for (auto c : line) {
+            newRow.push_back(c);
+            dirtyRow.push_back(false);
+        }
+        inputGrid.content.push_back(newRow);
+        dirtyGrid.content.push_back(dirtyRow);
+
+        rowIndex++;
+    }
+    std::cout << "Grid Dimensions: " << inputGrid.getWidth() << "w x " << inputGrid.getHeight() << "h" << std::endl;
         
-    int nextSymbolIndex = fileContent.find_first_not_of("'0123456789.");
-    while (nextSymbolIndex != std::string::npos) {
-        std::cout << "Next symbol index: " << nextSymbolIndex << std::endl;
-        nextSymbolIndex = fileContent.find_first_not_of("'0123456789.", nextSymbolIndex + 1);
-    }
+    /*std::set<int> visitedCells;
+    auto matchesBegin = std::sregex_iterator(fileContent.begin(), fileContent.end(), nonSymbolCharacters);
+    auto matchesEnd = std::sregex_iterator();
+    std::cout << "Found " << std::distance(matchesBegin, matchesEnd) << " symbols.\n";
+    for (auto iter = matchesBegin; iter != matchesEnd; iter++) {
+        std::smatch match = *iter;
+        std::cout << "Targetting symbol: " << match.str() << " at location " << match.position() << std::endl;
+    }*/
 
-    
-    
     
     std::cout << "Answer for Part 1 - Sum of part numbers: " << sumOfPartNumbers << std::endl;
 }
