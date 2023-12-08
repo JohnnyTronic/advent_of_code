e46cf85be5eb2643d155f10cccd4bbc244109c20
John <3317344+JohnnyTronic@users.noreply.github.com>
2023-12-08 12:32:21 -0500

    Day03 Part 1 success.

--- cpp/day03/day03.cpp
+++ cpp/day03/day03.cpp
@@ -19,82 +19,59 @@ public:
     }
 };
 
-template <typename T>
-class Grid2D {
-public:    
-    
-    std::vector<std::vector<T>> content;
-
-    int getWidth() {
-        return this->content.size() == 0 ? 0 : this->content[0].size() == 0 ? 0 : this->content[0].size();
-    }
-
-    int getHeight() {
-        return this->content.size();
-    }
-    
-    bool isOnGrid(Vec2& position) {
-        return position.x >= 0 && position.x < this->getWidth() && position.y >= 0 && position.y < this->getHeight();
-    }
-    
-    std::vector<Vec2> GenerateNeighbourPositions(const Vec2& position) {
-
-        std::vector<Vec2> possibleNeighbourPositions;
-
-        possibleNeighbourPositions.push_back(position + Vec2(-1, -1));
-        possibleNeighbourPositions.push_back(position + Vec2(0, -1));
-        possibleNeighbourPositions.push_back(position + Vec2(1, -1));
-        possibleNeighbourPositions.push_back(position + Vec2(-1, 0));
-        possibleNeighbourPositions.push_back(position + Vec2(1, 0));
-        possibleNeighbourPositions.push_back(position + Vec2(-1, 1));
-        possibleNeighbourPositions.push_back(position + Vec2(0, 1));
-        possibleNeighbourPositions.push_back(position + Vec2(1, 1));
-
-        std::vector<Vec2> onGridNeighbours;
-        for (auto& possibleNeighbour : possibleNeighbourPositions) {
-            if(this.isOnGrid(possibleNeighbour)) {
-                onGridNeighbours.push_back(possibleNeighbour);
-            }
-        }
-
-        return onGridNeighbours;
-    }
-};
-
-class PartNumber {
+class GridItem {
 public:
+    std::string str;
     int value;
     Vec2 gridPosition;
     int length;
 
-    PartNumber(int value, Vec2 gridPosition, int length) : value(value), gridPosition(gridPosition), length(length) {}
-};
+    GridItem(int value, Vec2 gridPosition, int length) : value(value), gridPosition(gridPosition), length(length) {}
+    GridItem(std::string str, Vec2 gridPosition) : str(str), value(0), gridPosition(gridPosition), length(1), isSymbol(true) {}
 
-class Symbol {
-public:
-    std::string value;
-    Vec2 gridPosition;
-
-    Symbol(std::string value, Vec2 gridPosition) : value(value), gridPosition(gridPosition) {}
-
-    friend std::ostream& operator<< (std::ostream& out, const Symbol& symbol) {
-        out << "Symbol[\'" << symbol.value << ", (" << symbol.gridPosition.x << "," << symbol.gridPosition.y << ")";
+    int minBoundX() {
+        return gridPosition.x;
     }
+
+    int maxBoundX() {
+        return gridPosition.x + length;
+    }
+
+    int minBoundY() {
+        return gridPosition.y;
+    }
+
+    int maxBoundY() {
+        return gridPosition.y + 1;
+    }
+
+    friend std::ostream& operator<< (std::ostream& out, const GridItem& gridItem) {
+        std::string output;
+        if (gridItem.isSymbol) {
+            output = gridItem.str;
+        }
+        else
+            output = std::to_string(gridItem.value);
+        out << "Grid item [" << output << "], (" << gridItem.gridPosition.x << "," << gridItem.gridPosition.y << ") length: " << gridItem.length;
+        return out;
+    }
+
+private:
+    bool isSymbol = false;
+    
 };
 
 int main()
 {
     std::cout << "Advent of Code 2023 - Day 3\n";
+        
+    std::vector<GridItem> partNumbers;
+    std::vector<GridItem> symbols;
 
-    std::vector<PartNumber> partNumbers;
-    int sumOfPartNumbers = 0;
-
-    std::ifstream ifs("test_input.txt");
-    //std::ifstream ifs("input.txt");
+    //std::ifstream ifs("test_input.txt");
+    std::ifstream ifs("input.txt");
     std::string line;    
-
-    Grid2D<char> inputGrid;
-    Grid2D<bool> dirtyGrid;
+ 
     std::regex partNumberPattern("[0-9]+");
     std::regex symbolPattern("[^0-9.]+");
     int rowIndex = 0;
@@ -104,11 +81,11 @@ int main()
         std::sregex_iterator partNumberMatchesEnd;
         for (auto iter = partNumberMatchesBegin; iter != partNumberMatchesEnd; iter++) {
             std::smatch partNumberMatch = *iter;
-            std::cout << "Found part number: " << partNumberMatch.str() << std::endl;
             auto partNumberString = partNumberMatch.str();
             auto partNumberValue = std::stoi(partNumberString);
-            PartNumber newPartNumber(partNumberValue, Vec2(partNumberMatch.position(), rowIndex), partNumberString.length());
+            GridItem newPartNumber(partNumberValue, Vec2(partNumberMatch.position(), rowIndex), partNumberString.length());
             partNumbers.push_back(newPartNumber);
+            std::cout << "Found part number: " << newPartNumber << std::endl;
         }
 
         // Search for Symbols
@@ -116,33 +93,42 @@ int main()
         std::sregex_iterator symbolMatchesEnd;
         for (auto iter = symbolMatchesBegin; iter != symbolMatchesEnd; iter++) {
             std::smatch symbolMatch = *iter;
-            std::cout << "Found symbol: " << symbolMatch.str() << std::endl;
             auto symbolString = symbolMatch.str();
-            Symbol newSymbol(symbolString, Vec2(symbolMatch.position(), rowIndex));
+            GridItem newSymbol(symbolString, Vec2(symbolMatch.position(), rowIndex));
+            symbols.push_back(newSymbol);
+            std::cout << "Found symbol: " << newSymbol << std::endl;
         }
-
-        std::vector<char> newRow;
-        std::vector<bool> dirtyRow;
-        for (auto c : line) {
-            newRow.push_back(c);
-            dirtyRow.push_back(false);
-        }
-        inputGrid.content.push_back(newRow);
-        dirtyGrid.content.push_back(dirtyRow);
 
         rowIndex++;
     }
-    std::cout << "Grid Dimensions: " << inputGrid.getWidth() << "w x " << inputGrid.getHeight() << "h" << std::endl;
-        
-    /*std::set<int> visitedCells;
-    auto matchesBegin = std::sregex_iterator(fileContent.begin(), fileContent.end(), nonSymbolCharacters);
-    auto matchesEnd = std::sregex_iterator();
-    std::cout << "Found " << std::distance(matchesBegin, matchesEnd) << " symbols.\n";
-    for (auto iter = matchesBegin; iter != matchesEnd; iter++) {
-        std::smatch match = *iter;
-        std::cout << "Targetting symbol: " << match.str() << " at location " << match.position() << std::endl;
-    }*/
+    std::cout << "Part Number Counts: " << partNumbers.size() << ", Symbols Count: " << symbols.size() << std::endl;
+      
+    // Collision Detection
+    std::vector<GridItem> collidingParts;
+    for (auto& partNumber : partNumbers) {
+        //std::cout << "Checking " << partNumber.value;
+        for (auto& symbol : symbols) {
+            //std::cout << " against " << symbol.str << std::endl;
+            if (partNumber.minBoundX() > symbol.maxBoundX())
+                continue;
+            if (partNumber.maxBoundX() < symbol.minBoundX())
+                continue;
+            if (partNumber.minBoundY() > symbol.maxBoundY())
+                continue;
+            if (partNumber.maxBoundY() < symbol.minBoundY())
+                continue;
+
+            collidingParts.push_back(partNumber);
+            std::cout << "Collision between " << partNumber.value << " & " << symbol.str << std::endl;
+        }
+    }
+    std::cout << "Colliding parts count: " << collidingParts.size() << std::endl;
+
+    int sumOfCollidingPartNumbers = 0;
+    for (auto& collidingPart : collidingParts) {
+        sumOfCollidingPartNumbers += collidingPart.value;
+    }
 
     
-    std::cout << "Answer for Part 1 - Sum of part numbers: " << sumOfPartNumbers << std::endl;
+    std::cout << "Answer for Part 1 - Sum of symbol-adjacent part numbers: " << sumOfCollidingPartNumbers << std::endl;
 }
