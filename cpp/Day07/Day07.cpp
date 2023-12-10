ff59335271aa5495359ef6e30c2625b5334638c7
John <3317344+JohnnyTronic@users.noreply.github.com>
2023-12-10 15:31:06 -0500

    Part 1 success.

--- cpp/Day07/Day07.cpp
+++ cpp/Day07/Day07.cpp
@@ -8,14 +8,33 @@
 
 using std::cout;
 
+int GetCardIndex(char label) {
+    switch (label) {
+    case '2': return 0;
+    case '3': return 1;
+    case '4': return 2;
+    case '5': return 3;
+    case '6': return 4;
+    case '7': return 5;
+    case '8': return 6;
+    case '9': return 7;
+    case 'T': return 8;
+    case 'J': return 9;
+    case 'Q': return 10;
+    case 'K': return 11;
+    case 'A': return 12;       
+    default: throw "Unrecognized card index: " + label;
+    }
+}
+
 enum class HandType {
-    FiveOfAKind,
-    FourOfAKind,
-    FullHouse,
-    ThreeOfAKind,
-    TwoPair,
+    HighCard,
     OnePair,
-    HighCard
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
 };
 
 static std::string GetHandTypeName(HandType& handType) {
@@ -82,10 +101,36 @@ void TestClassify(std::string& cards, HandType expectedResult) {
     std::cout << "TestClassify: " << cards << " -> " << GetHandTypeName(result) << ", Matches expected result?: " <<  matchesExpectedResult << std::endl;
 }
 
-int main()
-{
-    std::cout << "Advent of Code 2023 - Day 07!\n";
+bool SortTwoHands(Hand& left, Hand& right) {
+    HandType leftClass = ClassifyHand(left.cards);
+    HandType rightClass = ClassifyHand(right.cards);
+    int leftIndex = static_cast<int>(leftClass);
+    int rightIndex = static_cast<int>(rightClass);
 
+    if (leftIndex < rightIndex)
+        return true;
+
+    if (leftIndex > rightIndex)
+        return false;
+
+    if (leftIndex == rightIndex) {
+        for (int i = 0; i < left.cards.length(); i++) {
+            int leftCardIndex = GetCardIndex(left.cards[i]);
+            int rightCardIndex = GetCardIndex(right.cards[i]);
+            if (leftCardIndex < rightCardIndex)
+                return true;
+
+            if (leftCardIndex > rightCardIndex)
+                return false;
+
+            continue;
+        }
+
+        throw "Are these identical hands?";
+    }
+}
+
+void TestClassifier() {
     std::string fiveOfAKindHand("KKKKK");
     TestClassify(fiveOfAKindHand, HandType::FiveOfAKind);
 
@@ -106,11 +151,42 @@ int main()
 
     std::string highCardHand("54321");
     TestClassify(highCardHand, HandType::HighCard);
+}
+
+void TestCardIndexFinder() {
+    if (GetCardIndex('2') != 0)
+        throw "Index 2 failed";
+
+    if (GetCardIndex('A') != 12)
+        throw "Index A failed.";
+
+    std::cout << "TestCardIndexFinder SUCCESS";
+}
+
+void TestCardSort() {
+    Hand testHandA("KKKKK", 100);
+    Hand testHandB("AAAA2", 200);
+    Hand testHandC("22233", 300);
+
+    std::vector<Hand> hands { testHandA, testHandB, testHandC };
+    std::sort(hands.begin(), hands.end(), SortTwoHands);
+
+    bool result = SortTwoHands(testHandB, testHandA);
+    std::cout << "Sort test result: " << result << std::endl;
+}
+
+int main()
+{
+    std::cout << "Advent of Code 2023 - Day 07!\n";
+
+    //TestClassifier();
+    //TestCardIndexFinder();
+    //TestCardSort();
     
-    return 0;
 
     //std::ifstream ifs("test_input.txt");
     std::ifstream ifs("input.txt");
+
     std::string line;
     std::regex handPattern(R"(([\d\w]{5})\s+(\d+))");
     std::vector<Hand> hands;
@@ -124,6 +200,16 @@ int main()
         Hand newHand(cards, bid);
         hands.push_back(newHand);
     }
+
+    std::sort(hands.begin(), hands.end(), SortTwoHands);
+
+    // Calculate total score
+    long long totalScore = 0;
+    for (int i = 0; i < hands.size(); i++) {
+        totalScore += hands[i].bid * (i + 1);
+    }
+
+    std::cout << "PART 1 ANSWER - Total score of sorted hands: " << totalScore << std::endl;
 
     return 0;
 }
