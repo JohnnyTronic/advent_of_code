7ff47f6585f18e7ba2f1916ffb980b9370ec82d3
John <3317344+JohnnyTronic@users.noreply.github.com>
2023-12-11 03:14:32 -0500

    Day 07 Part 2 success
    Definitely the most specific set of regex patterns yet...

--- cpp/Day07/Day07.cpp
+++ cpp/Day07/Day07.cpp
@@ -8,22 +8,42 @@
 
 using std::cout;
 
-int GetCardIndex(char label) {
-    switch (label) {
-    case '2': return 0;
-    case '3': return 1;
-    case '4': return 2;
-    case '5': return 3;
-    case '6': return 4;
-    case '7': return 5;
-    case '8': return 6;
-    case '9': return 7;
-    case 'T': return 8;
-    case 'J': return 9;
-    case 'Q': return 10;
-    case 'K': return 11;
-    case 'A': return 12;       
-    default: throw "Unrecognized card index: " + label;
+int GetCardIndex(char label, bool useJokers) {
+    if (!useJokers) {
+        switch (label) {
+        case '2': return 0;
+        case '3': return 1;
+        case '4': return 2;
+        case '5': return 3;
+        case '6': return 4;
+        case '7': return 5;
+        case '8': return 6;
+        case '9': return 7;
+        case 'T': return 8;
+        case 'J': return 9;
+        case 'Q': return 10;
+        case 'K': return 11;
+        case 'A': return 12;       
+        default: throw "Unrecognized card index: " + label;
+        }
+    }
+    else {
+        switch (label) {
+        case 'J': return 0;
+        case '2': return 1;
+        case '3': return 2;
+        case '4': return 3;
+        case '5': return 4;
+        case '6': return 5;
+        case '7': return 6;
+        case '8': return 7;
+        case '9': return 8;
+        case 'T': return 9;
+        case 'Q': return 10;
+        case 'K': return 11;
+        case 'A': return 12;
+        default: throw "Unrecognized card index: " + label;
+        }
     }
 }
 
@@ -37,7 +57,7 @@ enum class HandType {
     FiveOfAKind,
 };
 
-static std::string GetHandTypeName(HandType& handType) {
+static std::string GetHandTypeName(const HandType& handType) {
     switch (handType) {
     case HandType::FiveOfAKind: return "FiveOfAKind";
     case HandType::FourOfAKind: return "FourOfAKind";
@@ -57,53 +77,109 @@ public:
     Hand(std::string cards, int bid) : cards(cards), bid(bid) {}
 };
 
-HandType ClassifyHand(std::string& cards) {
-    // Sort a copy of the input first (simplifies the regex work to have similar cards grouped)
-    std::string input = cards;
-    std::sort(input.begin(), input.end());
+bool SortTwoCards(char left, char right, bool useJokers) {
+    return GetCardIndex(left, useJokers) > GetCardIndex(right, useJokers);
+}
 
-    std::regex fiveOfAKindPattern(R"((\w)\1\1\1\1)");
-    if (std::regex_search(input, fiveOfAKindPattern)) {
-        return HandType::FiveOfAKind;
+HandType ClassifyHand(const std::string& cards, bool useJokers) {
+    if (!useJokers) {
+
+        // Sort a copy of the input first (simplifies the regex work to have similar cards grouped)
+        std::string input = cards;
+        std::sort(input.begin(), input.end(),
+            [](char leftCard, char rightCard) {
+                return SortTwoCards(leftCard, rightCard, false);
+            });
+
+        std::regex fiveOfAKindPattern(R"((\w)\1\1\1\1)");
+        if (std::regex_search(input, fiveOfAKindPattern)) {
+            return HandType::FiveOfAKind;
+        }
+
+        std::regex fourOfAKindPattern(R"((\w)\1\1\1)");
+        if (std::regex_search(input, fourOfAKindPattern)) {
+            return HandType::FourOfAKind;
+        }
+
+        std::regex fullHousePattern(R"((?:(\w)\1\1(\w)\2)|(?:(\w)\3(\w)\4\4))");
+        if (std::regex_search(input, fullHousePattern)) {
+            return HandType::FullHouse;
+        }
+
+        std::regex threeOfAKindPattern(R"(.*(\w)(?=.*\1.*\1))");
+        if (std::regex_search(input, threeOfAKindPattern)) {
+            return HandType::ThreeOfAKind;
+        }
+
+        std::regex twoPairPattern(R"(.*(\w)(?=.*\1).*(\w)(?=.*\2))");
+        if (std::regex_search(input, twoPairPattern)) {
+            return HandType::TwoPair;
+        }
+
+        std::regex onePairPattern(R"(.*(\w)(?=.*\1))");
+        if (std::regex_search(input, onePairPattern)) {
+            return HandType::OnePair;
+        }
+
+        return HandType::HighCard;
     }
+    else {
+        // Using jokers, regex patterns are more complicated
+        std::string input = cards;
+        std::sort(input.begin(), input.end(), [](char left, char right) {
+            return SortTwoCards(left, right, true);
+            });
 
-    std::regex fourOfAKindPattern(R"((\w)\1\1\1)");
-    if (std::regex_search(input, fourOfAKindPattern)) {
-        return HandType::FourOfAKind;
+        std::regex fiveOfAKindPattern(R"((\w)(?:\1|J)(?:\1|J)(?:\1|J)(?:\1|J))");
+        if (std::regex_search(input, fiveOfAKindPattern)) {
+            return HandType::FiveOfAKind;
+        }
+
+        std::regex fourOfAKindPattern(R"((\w).*(?:\1|J).*(?:\1|J).*(?:\1|J))");
+        if (std::regex_search(input, fourOfAKindPattern)) {
+            return HandType::FourOfAKind;
+        }
+
+        std::regex fullHousePattern(R"((?:(\w)\1\1(\w)(?:\2|J))|(?:(\w)\3(\w)(?:\4|J)(?:\4|J)))");
+        if (std::regex_search(input, fullHousePattern)) {
+            return HandType::FullHouse;
+        }
+
+        std::regex threeOfAKindPattern(R"(.*(\w)(?=.*(?:\1|J).*(?:\1|J)))");
+        if (std::regex_search(input, threeOfAKindPattern)) {
+            return HandType::ThreeOfAKind;
+        }
+
+        std::regex twoPairPattern(R"((?:.*(\w)(?=.*(?:\1|J)).*(\w)(?=.*(?:\2)))|(?:.*(\w)(?=.*(?:\3)).*(\w)(?=.*(?:\4|J))))");
+        if (std::regex_search(input, twoPairPattern)) {
+            return HandType::TwoPair;
+        }
+
+        std::regex onePairPattern(R"(.*(\w).*(\1|J))");
+        if (std::regex_search(input, onePairPattern)) {
+            return HandType::OnePair;
+        }
+
+        return HandType::HighCard;
     }
+}
 
-    std::regex fullHousePattern(R"((?:(\w)\1\1(\w)\2)|(?:(\w)\3(\w)\4\4))");
-    if (std::regex_search(input, fullHousePattern)) {
-        return HandType::FullHouse;
-    }
+bool SortTwoCardsWithJokers(char left, char right) {
+    if (left == 'J')
+        return true;
 
-    std::regex threeOfAKindPattern(R"(.*(\w)(?=.*\1.*\1))");
-    if (std::regex_search(input, threeOfAKindPattern)) {
-        return HandType::ThreeOfAKind;
-    }
-
-    std::regex twoPairPattern(R"(.*(\w)(?=.*\1).*(\w)(?=.*\2))");
-    if (std::regex_search(input, twoPairPattern)) {
-        return HandType::TwoPair;
-    }
-
-    std::regex onePairPattern(R"(.*(\w)(?=.*\1))");
-    if (std::regex_search(input, onePairPattern)) {
-        return HandType::OnePair;
-    }
-
-    return HandType::HighCard;
+    return left < right;
 }
 
 void TestClassify(std::string& cards, HandType expectedResult) {
-    HandType result = ClassifyHand(cards);
+    HandType result = ClassifyHand(cards, false);
     std::string matchesExpectedResult = expectedResult == result ? "CORRECT" : "WRONG";
     std::cout << "TestClassify: " << cards << " -> " << GetHandTypeName(result) << ", Matches expected result?: " <<  matchesExpectedResult << std::endl;
 }
 
-bool SortTwoHands(Hand& left, Hand& right) {
-    HandType leftClass = ClassifyHand(left.cards);
-    HandType rightClass = ClassifyHand(right.cards);
+bool SortTwoHands(Hand& leftHand, Hand& rightHand, bool useJokers) {
+    HandType leftClass = ClassifyHand(leftHand.cards, useJokers);
+    HandType rightClass = ClassifyHand(rightHand.cards, useJokers);
     int leftIndex = static_cast<int>(leftClass);
     int rightIndex = static_cast<int>(rightClass);
 
@@ -114,9 +190,9 @@ bool SortTwoHands(Hand& left, Hand& right) {
         return false;
 
     if (leftIndex == rightIndex) {
-        for (int i = 0; i < left.cards.length(); i++) {
-            int leftCardIndex = GetCardIndex(left.cards[i]);
-            int rightCardIndex = GetCardIndex(right.cards[i]);
+        for (int i = 0; i < leftHand.cards.length(); i++) {
+            int leftCardIndex = GetCardIndex(leftHand.cards[i], useJokers);
+            int rightCardIndex = GetCardIndex(rightHand.cards[i], useJokers);
             if (leftCardIndex < rightCardIndex)
                 return true;
 
@@ -154,10 +230,10 @@ void TestClassifier() {
 }
 
 void TestCardIndexFinder() {
-    if (GetCardIndex('2') != 0)
+    if (GetCardIndex('2', false) != 0)
         throw "Index 2 failed";
 
-    if (GetCardIndex('A') != 12)
+    if (GetCardIndex('A', false) != 12)
         throw "Index A failed.";
 
     std::cout << "TestCardIndexFinder SUCCESS";
@@ -169,20 +245,75 @@ void TestCardSort() {
     Hand testHandC("22233", 300);
 
     std::vector<Hand> hands { testHandA, testHandB, testHandC };
-    std::sort(hands.begin(), hands.end(), SortTwoHands);
+    std::sort(hands.begin(), hands.end(), [](auto& left, auto& right) {return SortTwoHands(left, right, false); });
 
-    bool result = SortTwoHands(testHandB, testHandA);
+    bool result = SortTwoHands(testHandB, testHandA, false);
     std::cout << "Sort test result: " << result << std::endl;
+}
+
+void SolvePart1(std::vector<Hand>& hands) {
+    std::vector<Hand> sortedHands = hands;
+    std::sort(sortedHands.begin(), sortedHands.end(), [](auto& left, auto& right) {return SortTwoHands(left, right, false); });
+
+    // Calculate total score
+    long long totalScore = 0;
+    for (int i = 0; i < sortedHands.size(); i++) {
+        totalScore += sortedHands[i].bid * (i + 1);
+    }
+
+    std::cout << "PART 1 ANSWER - Total score of sorted hands: " << totalScore << std::endl;
+}
+
+void SolvePart2(std::vector<Hand>& hands) {
+    // Part 2 - Now with Jokers (wild cards)
+    std::vector<Hand> sortedHandsWithJokers = hands;
+    std::sort(sortedHandsWithJokers.begin(), sortedHandsWithJokers.end(), [](Hand& left, Hand& right) {
+        return SortTwoHands(left, right, true);
+        });
+
+    int index = 0;
+    for (const Hand& hand : sortedHandsWithJokers) {
+        auto& cards = hand.cards;
+        std::cout << index << " - " << hand.cards << " - " << GetHandTypeName(ClassifyHand(cards, true)) << ", bid " << hand.bid << std::endl;
+        index++;
+    }
+    
+    // Calculate total score
+    long long totalScoreWithJokers = 0;
+    for (int i = 0; i < sortedHandsWithJokers.size(); i++) {
+        totalScoreWithJokers += sortedHandsWithJokers[i].bid * (i + 1);
+    }
+
+    std::cout << "PART 2 ANSWER - Total score of sorted hands including jokers: " << totalScoreWithJokers << std::endl;
+}
+
+void ExpectClassification(const Hand& hand, const HandType expectedHandType, bool useJokers) {
+    auto result = ClassifyHand(hand.cards, useJokers);
+    if (result != expectedHandType) {
+        std::cout << "Given hand " << hand.cards << ", expected " << GetHandTypeName(expectedHandType) << " but got " << GetHandTypeName(result) << std::endl;
+        throw;
+    }
 }
 
 int main()
 {
     std::cout << "Advent of Code 2023 - Day 07!\n";
 
-    //TestClassifier();
-    //TestCardIndexFinder();
-    //TestCardSort();
-    
+    Hand handA("J39Q5", 100);
+    Hand handB("2398Q", 200);
+    Hand handC("24972", 300);
+    Hand handD("2T7T7", 400);
+    Hand handE("JT7T7", 500);
+    Hand handF("TT7TJ", 500); // Was fullhouse should be 4ofakind
+    //std::vector<Hand> testHands{ handA, handB, handC, handD, handE };
+    //std::sort(testHands.begin(), testHands.end(), [](auto& left, auto& right) {return SortTwoHands(left, right, true); });
+    ////bool result = SortTwoHands(handA, handB, true);
+    ExpectClassification(handA, HandType::OnePair, true);
+    ExpectClassification(handB, HandType::HighCard, true);
+    ExpectClassification(handC, HandType::OnePair, true);
+    ExpectClassification(handD, HandType::TwoPair, true);
+    ExpectClassification(handE, HandType::FullHouse, true);
+    //return 0;
 
     //std::ifstream ifs("test_input.txt");
     std::ifstream ifs("input.txt");
@@ -201,15 +332,8 @@ int main()
         hands.push_back(newHand);
     }
 
-    std::sort(hands.begin(), hands.end(), SortTwoHands);
-
-    // Calculate total score
-    long long totalScore = 0;
-    for (int i = 0; i < hands.size(); i++) {
-        totalScore += hands[i].bid * (i + 1);
-    }
-
-    std::cout << "PART 1 ANSWER - Total score of sorted hands: " << totalScore << std::endl;
-
+    //SolvePart1(hands);
+    SolvePart2(hands);
+    
     return 0;
 }
