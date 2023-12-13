edd6791927fcfd6d6dfe175e6e142133e591d7f1
John <3317344+JohnnyTronic@users.noreply.github.com>
2023-12-13 15:37:16 -0500

    More power!!! with threads...

--- cpp/Day12/Day12.cpp
+++ cpp/Day12/Day12.cpp
@@ -6,6 +6,7 @@
 #include <set>
 #include <regex>
 #include <map>
+#include <thread>
 
 // Example Regex pattern for 3,2,1 - [\.\?]*[#\?]{3}[\.\?]+[#\?]{2}[\.\?]+[#\?]{1}[\.\?]*
 
@@ -68,39 +69,75 @@ int main()
 
     long long totalPossibilities{};
 
-    //std::ifstream ifs("test_input.txt");
-    std::ifstream ifs("input.txt");
+    std::ifstream ifs("test_input.txt");
+    //std::ifstream ifs("input.txt");
 
     std::regex springsChunkPattern(R"([\?.#]+(?=\s))");
     std::regex legendChunkPattern(R"(\s[,\d]+)");
     std::regex legendNumberPattern(R"((\d+))");
     std::string parsedLine;
+    std::vector<std::string> conditionChunks;
+    std::vector<std::string> legendChunks;
     while (ifs.good()) {
         std::getline(ifs, parsedLine);
-        
+            
         std::smatch springsChunkMatch;
         std::regex_search(parsedLine, springsChunkMatch, springsChunkPattern);
         std::string springsChunk = springsChunkMatch.str();
-        
+        conditionChunks.push_back(springsChunk);
+
         std::smatch legendChunkMatch;
         std::regex_search(parsedLine, legendChunkMatch, legendChunkPattern);
 
         std::string legendChunk = legendChunkMatch.str();
         std::regex spaceChopperPattern(R"(^\s*)");
         legendChunk = std::regex_replace(legendChunk, spaceChopperPattern, "");
+        legendChunks.push_back(legendChunk);
         
-        auto legendRegex = CreateRegexForLegendNumbers(legendChunk);
+        //auto legendRegex = CreateRegexForLegendNumbers(legendChunk);
 
-        // Sanity check: does legendRegex match against the root springs chunk?
-        bool didMatch = std::regex_match(springsChunk, legendRegex);
-        if (!didMatch)
-            throw;
+        //// Sanity check: does legendRegex match against the root springs chunk?
+        //bool didMatch = std::regex_match(springsChunk, legendRegex);
+        //if (!didMatch)
+        //    throw;
 
-        auto possibilities = GenerateSpringPossibilities(springsChunk, legendRegex);
-        totalPossibilities += possibilities.size();
+        //auto possibilities = GenerateSpringPossibilities(springsChunk, legendRegex);
+        //totalPossibilities += possibilities.size();
     }
 
     std::cout << "PART 1 ANSWER - Total possible arrangements: " << totalPossibilities << "\n";
+
+    // Part 2 - Unfold the damned instructions!
+    long long expandedTotalPossibilities = 0;
+    std::vector<std::thread> threads;
+    for (int i = 0; i < legendChunks.size(); i++) {
+        std::string conditionChunk = conditionChunks[i];
+        std::string legendChunk = legendChunks[i];
+
+        std::string expandedConditionChunk = conditionChunk;
+        std::string expandedLegendChunk = legendChunk;
+        for (int r = 0; r < 4; r++) {
+            expandedConditionChunk += "?" + conditionChunk;
+            expandedLegendChunk += "," + legendChunk;
+        }
+
+        std::cout << "Expanded chunks: " << expandedConditionChunk << ", " << expandedLegendChunk << "\n";
+
+        auto legendRegex = CreateRegexForLegendNumbers(expandedLegendChunk);
+
+        // Sanity check: does legendRegex match against the root springs chunk?
+        bool didMatch = std::regex_match(expandedConditionChunk, legendRegex);
+        if (!didMatch)
+            throw;
+
+        std::thread([](auto& chunk, auto& regex) {
+            auto possibilities = GenerateSpringPossibilities(expandedConditionChunk, legendRegex);
+            expandedTotalPossibilities += possibilities.size();
+            }, expandedConditionChunk, legendRegex);
+        
+    }
+
+    std::cout << "PART 2 ANSWER - Total possibilities for expanded instruction set: " << expandedTotalPossibilities << "\n";
 
     return 0;
 }
