442253e230cc0eca36fb602bc2efebf51506ee23
John <3317344+JohnnyTronic@users.noreply.github.com>
2023-12-25 22:39:59 -0500

    Day 18 Part 1 success.

--- cpp/Day18/Day18.cpp
+++ cpp/Day18/Day18.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <regex>
 #include <map>
+#include <cassert>
 
 enum class Direction {
     NORTH,
@@ -64,22 +65,22 @@ Extents GetExtents(const std::vector<Vec2>& vertices) {
 class Cell {
 public:
     bool isHollow{ false };
+    bool isTrench{ false };
     std::string northColor{ "" };
     std::string eastColor{ "" };
     std::string southColor{ "" };
     std::string westColor{ "" };
 };
 
-template <typename T>
 class Grid {
 public:
-    std::map<int, std::map<int, T>> content;
+    std::map<int, std::map<int, Cell*>> content;
 
-    T Get(Vec2 position) {   
+    Cell* Get(Vec2 position) {   
         return content[position.y][position.x];
     }
 
-    void Set(Vec2 position, T cell) {
+    void Set(Vec2 position, Cell* cell) {
         content[position.y][position.x] = cell;
     }
 
@@ -92,7 +93,12 @@ public:
                     std::cout << ".";
                     continue;
                 }
-                std::cout << cell->isHollow ? "#" : ".";
+                if (cell->isTrench)
+                    std::cout << "#";
+                else if (cell->isHollow)
+                    std::cout << "X";
+                else
+                    std::cout << ".";                
             }
             std::cout << "\n";
         }
@@ -108,7 +114,7 @@ public:
     Edge(Vec2 vertexA, Vec2 vertexB) : vertexA(vertexA), vertexB(vertexB) {};
 };
 
-void PaintCell(Cell* cell, Instruction& instruction) {
+void PaintCell(Cell* cell, const Instruction& instruction) {
     if (instruction.direction == Direction::NORTH || instruction.direction == Direction::SOUTH) {
         cell->eastColor = instruction.hexColor;
         cell->westColor = instruction.hexColor;
@@ -119,11 +125,99 @@ void PaintCell(Cell* cell, Instruction& instruction) {
     }
 }
 
+Grid CutTrench(const std::vector<Instruction>& instructions) {
+    Grid grid;
+    Vec2 currentPosition{};
+    for (auto& instruction : instructions) {
+        Vec2 direction = DirectionToVec2(instruction.direction);
+        Vec2 startPosition = currentPosition;
+        for (int i = 0; i <= instruction.distance; i++) {
+            currentPosition = startPosition + direction * i;
+            Cell* cell = grid.Get(currentPosition);
+            if (cell == nullptr) {
+                cell = new Cell();
+                grid.Set(currentPosition, cell);
+            }           
+            cell->isHollow = true;
+            cell->isTrench = true;
+            PaintCell(cell, instruction);
+        }
+    }
+
+    return grid;
+}
+
+bool IsDegenerateCorner(Vec2 scanPos, Grid& grid) {
+    Cell* scanCell = grid.Get(scanPos);
+    if (scanCell == nullptr || !scanCell->isTrench)
+        return false;
+
+    Cell* northNeighbour = grid.Get(scanPos + Vec2(0, -1));
+    Cell* eastNeighbour = grid.Get(scanPos + Vec2(1, 0));
+    if (northNeighbour != nullptr && northNeighbour->isTrench && eastNeighbour != nullptr && eastNeighbour->isTrench)
+        return true;
+
+    Cell* westNeighbour = grid.Get(scanPos + Vec2(-1, 0));
+    Cell* southNeighbour = grid.Get(scanPos + Vec2(0, 1));
+    if (westNeighbour != nullptr && westNeighbour->isTrench && southNeighbour != nullptr && southNeighbour->isTrench)
+        return true;
+
+    return false;
+}
+
+long long ScanDiagonalDownRight(Vec2 scanStartPos, Grid& grid, Extents& extents) {
+    Vec2 scanDirection(1, 1);
+    bool isInterior = false;   
+    Vec2 scanPos = scanStartPos;
+    long long totalInteriorCells = 0;
+    while (scanPos.x <= extents.xMax && scanPos.y <= extents.yMax + 1) {
+        Cell* scanCell = grid.Get(scanPos);
+
+        if (scanCell != nullptr && scanCell->isTrench){
+            totalInteriorCells++;
+            if (!IsDegenerateCorner(scanPos, grid)) {
+                isInterior = !isInterior;
+            }            
+        }
+        else if (scanCell == nullptr && isInterior) {
+            Cell* interiorCell = new Cell();
+            interiorCell->isHollow = true;
+            grid.Set(scanPos, interiorCell);
+            totalInteriorCells++;
+        }
+        else if (scanCell != nullptr && isInterior) {
+            scanCell->isHollow = true;
+            totalInteriorCells++;
+        }
+        scanPos = scanPos + scanDirection;
+    }
+    return totalInteriorCells;
+}
+
+long long FillTrenchInterior(Grid& grid, Extents& extents) {
+
+    // With a slight overscan, travel up the left edge then across the top edge of the extents
+    // At each step, scan diagonally down-right, looking for trench crossings
+    // and noting "degenerate" corners 7 and L
+    long long totalHollowCells = 0;
+    for (int y = extents.yMax + 1; y >= extents.yMin - 1; y--) {
+        Vec2 scanStartPos(extents.xMin - 1, y);
+        totalHollowCells += ScanDiagonalDownRight(scanStartPos, grid, extents);
+    }
+
+    for (int x = extents.xMin; x <= extents.xMax; x++) {
+        Vec2 scanStartPos(x, extents.yMin - 1);
+        totalHollowCells += ScanDiagonalDownRight(scanStartPos, grid, extents);
+    }
+
+    return totalHollowCells;
+}
+
 int main()
 {
     std::cout << "Advent of Code - Day 18!\n";
 
-    std::ifstream ifs("test_input.txt");
+    std::ifstream ifs("input.txt");
     std::string parsedLine;
     std::vector<std::string> parsedLines;
     while (ifs.good()) {
@@ -165,25 +259,11 @@ int main()
     Edge finalEdge(vertices.back(), vertices.front());
     edges.push_back(finalEdge);
 
-    Grid<Cell*> grid;
-    Vec2 currentPosition{};    
-    for (auto& instruction : instructions) {
-        Vec2 direction = DirectionToVec2(instruction.direction);
-        Vec2 startPosition = currentPosition;
-        for (int i = 0; i <= instruction.distance; i++) {
-            currentPosition = startPosition + direction * i;            
-            Cell* cell = grid.Get(currentPosition);
-            if (cell == nullptr) {
-                cell = new Cell();
-                grid.Set(currentPosition, cell);
-            }               
-            cell->isHollow = true;
-            PaintCell(cell, instruction);
-        }
-    }
-    
+    Grid grid = CutTrench(instructions);    
     grid.Print(extents);
-    
+
+    lagoonVolume = FillTrenchInterior(grid, extents);    
+    grid.Print(extents);
 
     std::cout << "ANSWER PART 1 - What is the lagoon's volume?: " << std::to_string(lagoonVolume) << "\n";
     return 0;
