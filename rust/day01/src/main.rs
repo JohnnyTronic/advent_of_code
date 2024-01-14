602123161b7fcaa3460cd3efe328589f3898fac1
ArchGLaDOS <>
2024-01-14 15:44:43 -0500

    Day 01, this time in Rust!

---
+++ rust/day01/src/main.rs
@@ -0,0 +1,18 @@
+use std::fs::read_to_string;
+
+fn main() {
+    println!("Advent of Code - Day 01!");
+
+    read_lines("testInput.txt");
+}
+
+fn read_lines(filename: &str) -> Vec<String> {
+    let mut result = Vec::new();
+
+    for line in read_to_string(filename).unwrap().lines() {
+        result.push(line.to_string());
+        println!("{}", line.to_string());
+    }
+
+    result
+}
