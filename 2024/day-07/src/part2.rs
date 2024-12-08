f9ca976bfd22cd97b39cd65af691d97407a7b8f6
John <john@theplayfulpixel.ca>
2024-12-07 21:43:05 -0500

    Day 07 Part 2 success thanks to Joanna

--- 2024/day-07/src/part2.rs
+++ 2024/day-07/src/part2.rs
@@ -6,7 +6,6 @@ pub fn process(input: &str) -> std::result::Result<String, Error> {
     for line in input.lines() {
         let mut line_split = line.split(':');
         let test_value = line_split.next().unwrap().parse::<u64>().unwrap();
-        // println!("Line sum: {}", target_sum);
 
         let args_list = line_split.next().expect("Error extracting args list");
         let args: Vec<u64> = args_list
@@ -16,20 +15,29 @@ pub fn process(input: &str) -> std::result::Result<String, Error> {
             .collect();
 
         let operator_count = args.len() - 1;
-        let total_permutations = 3_u64.pow(operator_count as u32);
+        let operators_sequence_permutations = generate_operator_sequences(operator_count as u64);
 
-        for perm in 0..total_permutations {
+        for operator_sequence in operators_sequence_permutations {
             let mut arg_iter = args.iter().enumerate();
-            let (_arg_index, starting_value) = arg_iter.next().expect("Empty args?!");
-            let mut running_total: u64 = *starting_value;
+            let (_arg_index, first_arg) = arg_iter.next().expect("Empty args?!");
+            let mut running_total: u64 = *first_arg;
 
-            for (next_arg_index, next_arg) in arg_iter {
-                let slot_mask = 1 << (next_arg_index - 1);
-                let extracted_value = perm & slot_mask;
-                if extracted_value > 0 {
-                    running_total += next_arg;
-                } else {
-                    running_total *= next_arg;
+            for (operator_index, operator) in operator_sequence.iter().enumerate() {
+                let (_next_arg_index, next_arg) =
+                    arg_iter.next().expect("Arg vs Operator count mismatch");
+
+                match operator {
+                    Operator::Add => {
+                        running_total += next_arg;
+                    }
+                    Operator::Multiply => {
+                        running_total *= next_arg;
+                    }
+                    Operator::Concatenate => {
+                        running_total = (running_total.to_string() + &next_arg.to_string())
+                            .parse::<u64>()
+                            .expect("Could re-numberatize the concatenated arg strings");
+                    }
                 }
             }
 
@@ -43,36 +51,49 @@ pub fn process(input: &str) -> std::result::Result<String, Error> {
     Ok(total_calibration_result.to_string())
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 enum Operator {
     Add,
     Multiply,
     Concatenate,
 }
 
-fn generate_operator_sequence(operator_count: u64) {}
+fn generate_operator_sequences(operator_count: u64) -> Vec<Vec<Operator>> {
+    let mut operator_chain_permutations: Vec<Vec<Operator>> = vec![
+        vec![Operator::Add],
+        vec![Operator::Multiply],
+        vec![Operator::Concatenate],
+    ];
+
+    if operator_count > 1 {
+        operator_chain_permutations =
+            append_operators(operator_chain_permutations, operator_count - 1);
+    }
+    operator_chain_permutations
+}
 
 fn append_operators(
     operator_chain_permutations: Vec<Vec<Operator>>,
-    depth: usize,
+    depth: u64,
 ) -> Vec<Vec<Operator>> {
-    if depth == 0 {
-        return operator_chain_permutations;
+    let mut to_return: Vec<Vec<Operator>> = vec![];
+
+    for permutation in operator_chain_permutations {
+        let mut add_variant = permutation.clone();
+        add_variant.push(Operator::Add);
+        to_return.push(add_variant);
+        let mut mul_variant = permutation.clone();
+        mul_variant.push(Operator::Multiply);
+        to_return.push(mul_variant);
+        let mut concat_variant = permutation.clone();
+        concat_variant.push(Operator::Concatenate);
+        to_return.push(concat_variant);
     }
 
-    for index in (0..operator_chain_permutations.len()).rev() {}
-    let mut variants: Vec<Vec<Operator>> = vec![];
-    let mut add_variant = operator_chain_permutations.to_vec();
-    add_variant.push(Operator::Add);
-    variants.push(add_variant);
-    let mut mul_variant = operator_chain_permutations.to_vec();
-    mul_variant.push(Operator::Add);
-    variants.push(mul_variant);
-    let mut concat_variant = operator_chain_permutations.to_vec();
-    concat_variant.push(Operator::Add);
-    variants.push(concat_variant);
-
-    variants
+    if depth > 1 {
+        to_return = append_operators(to_return, depth - 1)
+    };
+    to_return
 }
 
 #[cfg(test)]
