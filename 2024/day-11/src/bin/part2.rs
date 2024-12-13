fda8317554021347a57c978bedf7a4b28a701b50
John <3317344+JohnnyTronic@users.noreply.github.com>
2024-12-12 21:26:29 -0500

    Day 11 Part 2 success

--- 2024/day-11/src/bin/part2.rs
+++ 2024/day-11/src/bin/part2.rs
@@ -3,8 +3,8 @@ use std::io::Error;
 use day_11::part2::process;
 
 fn main() -> Result<(), Error> {
-    // let file = include_str!("input.txt");
-    let file = include_str!("input2.txt");
+    let file = include_str!("input.txt");
+    // let file = include_str!("input2.txt");
     let result = process(file)?;
     println!("Part 2 solution: {}", result);
     Ok(())
--- 2024/day-11/src/part2.rs
+++ 2024/day-11/src/part2.rs
@@ -1,13 +1,25 @@
-use std::{collections::HashMap, io::Error};
+use std::sync::{LazyLock, Mutex};
+use std::{
+    cell::RefCell,
+    collections::HashMap,
+    io::Error,
+    rc::{Rc, Weak},
+};
+
+static COMPUTE_MAP: LazyLock<Mutex<HashMap<String, usize>>> =
+    LazyLock::new(|| Mutex::new(HashMap::new()));
 
 #[derive(Debug)]
-struct MetaStone {
+struct Stone {
     value: usize,
     depth: usize,
-    final_count: Option<usize>,
+    cumulative_child_count: Option<usize>,
+    parent_stone: Option<Weak<RefCell<Stone>>>,
+    child_stone_a: Option<Rc<RefCell<Stone>>>,
+    child_stone_b: Option<Rc<RefCell<Stone>>>,
 }
 
-impl MetaStone {
+impl Stone {
     pub fn hash(&self) -> String {
         hash_metastone_values(self.value, self.depth)
     }
@@ -24,109 +36,103 @@ pub fn process(input: &str) -> std::result::Result<String, Error> {
         .map(|i| i.trim().parse::<usize>().expect("Parse error"))
         .collect();
 
-    let mut compute_map: HashMap<usize, (usize, Option<usize>)> = HashMap::new();
-    let mut resolved_metastones: HashMap<String, MetaStone> = HashMap::new();
-    let mut unresolved_metastones: Vec<MetaStone> = original_stone_values
+    // let mut compute_map: HashMap<usize, (usize, Option<usize>)> = HashMap::new();
+    // let mut resolved_stones: HashMap<String, Stone> = HashMap::new();
+    let unresolved_stones: Vec<Rc<RefCell<Stone>>> = original_stone_values
         .iter()
-        .map(|original_stone_value| MetaStone {
-            value: *original_stone_value,
-            depth: 0,
-            final_count: None,
+        .map(|original_stone_value| {
+            Rc::new(RefCell::new(Stone {
+                value: *original_stone_value,
+                depth: 0,
+                cumulative_child_count: None,
+                parent_stone: None,
+                child_stone_a: None,
+                child_stone_b: None,
+            }))
         })
         .collect();
 
-    let target_depth = 75;
-
-    while !unresolved_metastones.is_empty() {
-        unresolved_metastones.sort_by(|a, b| a.depth.cmp(&b.depth));
-        let mut metastone = unresolved_metastones.pop().unwrap();
-        println!("Investigating unresolve stone {:?}", &metastone);
-
-        if metastone.depth == target_depth {
-            println!("HIT FINAL DEPTH: {:?}", metastone);
-            metastone.final_count = Some(1);
-            resolved_metastones.insert(metastone.hash(), metastone);
-            continue;
-        }
-
-        let next_depth = metastone.depth + 1;
-        let (next_value_a, next_value_b) = compute_next_values(&mut compute_map, metastone.value);
-
-        let mut next_metastone_a: Option<&MetaStone> = None;
-        let mut next_metastone_b: Option<&MetaStone> = None;
-
-        if let Some(resolved_metastone_a) =
-            resolved_metastones.get(&hash_metastone_values(next_value_a, next_depth))
-        {
-            next_metastone_a = Some(resolved_metastone_a);
-            continue;
-        } else {
-            unresolved_metastones.push(MetaStone {
-                value: next_value_a,
-                depth: next_depth,
-                final_count: None,
-            });
-        };
-
-        if let Some(next_value_b) = next_value_b {
-            if let Some(resolved_metastone_b) =
-                resolved_metastones.get(&hash_metastone_values(next_value_b, next_depth))
-            {
-                next_metastone_b = Some(resolved_metastone_b);
-            } else {
-                unresolved_metastones.push(MetaStone {
-                    value: next_value_b,
-                    depth: next_depth,
-                    final_count: None,
-                });
-            };
-        }
-
-        if next_metastone_a.is_some_and(|i| i.final_count.is_some())
-            && next_metastone_b.is_some_and(|i| i.final_count.is_some())
-        {
-            let final_count_a = next_metastone_a.unwrap().final_count.unwrap();
-            let mut final_count_b = 0;
-
-            if next_metastone_b.is_some_and(|i| i.final_count.is_some()) {
-                final_count_b = next_metastone_b.unwrap().final_count.unwrap();
-            }
-
-            metastone.final_count = Some(final_count_a + final_count_b);
-            resolved_metastones.insert(metastone.hash(), metastone);
-        } else {
-            unresolved_metastones.push(metastone);
-        }
-    }
-
-    // dbg!(&unresolved_metastones, &resolved_metastones);
-    println!("Resolved count: {}", resolved_metastones.len());
-
+    // let target_depth = 75;
     let mut total_stones = 0;
-    for original_stone_value in original_stone_values {
-        if let Some(resolved_metastone) =
-            resolved_metastones.get(&hash_metastone_values(original_stone_value, 0))
-        {
-            total_stones += resolved_metastone.final_count.unwrap();
-        } else {
-            panic!("Could not find Resolved MetaStone!")
-        };
+
+    for stone in unresolved_stones {
+        let total_child_count = compute_cumulative_child_count(stone);
+        total_stones += total_child_count;
     }
 
     Ok(total_stones.to_string())
 }
 
-fn compute_next_values(
-    compute_map: &mut HashMap<usize, (usize, Option<usize>)>,
-    value: usize,
-) -> (usize, Option<usize>) {
-    if let Some(precomputed) = compute_map.get(&value) {
+fn compute_cumulative_child_count(stone: Rc<RefCell<Stone>>) -> usize {
+    let mut child_totals = 0;
+
+    if stone.borrow().depth == 75 {
+        println!("Bottomed out");
+        return 1;
+    }
+
+    let hash = &stone.borrow().hash();
+    if let Some(precomputed) = COMPUTE_MAP.lock().unwrap().get(hash) {
+        println!(
+            "Returning precomputed score [{}] for hash [{}]",
+            precomputed, hash
+        );
         return *precomputed;
     }
-    println!("Cache miss: {}", &value);
-    if value == 0 {
+
+    let (a_value, b_value) = compute_next_values(&stone.borrow().value);
+
+    let child_stone_a = Rc::new(RefCell::new(Stone {
+        value: a_value,
+        depth: stone.borrow().depth + 1,
+        cumulative_child_count: None,
+        parent_stone: Some(Rc::downgrade(&stone)),
+        child_stone_a: None,
+        child_stone_b: None,
+    }));
+    stone.borrow_mut().child_stone_a = Some(Rc::clone(&child_stone_a));
+
+    println!("Recurse A - Depth {}", child_stone_a.borrow().depth);
+    let stone_a_totals = compute_cumulative_child_count(child_stone_a);
+    child_totals += stone_a_totals;
+
+    if let Some(b_value) = b_value {
+        let child_stone_b = Rc::new(RefCell::new(Stone {
+            value: b_value,
+            depth: stone.borrow().depth + 1,
+            cumulative_child_count: None,
+            parent_stone: Some(Rc::downgrade(&stone)),
+            child_stone_a: None,
+            child_stone_b: None,
+        }));
+        stone.borrow_mut().child_stone_b = Some(Rc::clone(&child_stone_b));
+
+        println!("Recurse B - Depth {}", child_stone_b.borrow().depth);
+        let stone_b_totals = compute_cumulative_child_count(child_stone_b);
+        child_totals += stone_b_totals;
+    }
+
+    let hash = stone.borrow().hash();
+    println!(
+        "Recording precomputed score [{}] for hash [{}]",
+        child_totals, hash
+    );
+    COMPUTE_MAP.lock().unwrap().insert(hash, child_totals);
+
+    child_totals
+}
+
+fn compute_next_values(
+    // compute_map: &mut HashMap<usize, (usize, Option<usize>)>,
+    value: &usize,
+) -> (usize, Option<usize>) {
+    // if let Some(precomputed) = compute_map.get(value) {
+    //     return *precomputed;
+    // }
+    // println!("Cache miss: {}", &value);
+    if *value == 0 {
         let answer = (1, None);
-        compute_map.insert(value, answer);
+        // compute_map.insert(*value, answer);
         return answer;
     }
 
@@ -137,13 +143,12 @@ fn compute_next_values(
         let first_num = first.parse::<usize>().unwrap();
         let second_num = second.parse::<usize>().unwrap();
         let answer = (first_num, Some(second_num));
-        compute_map.insert(value, answer);
+        // compute_map.insert(*value, answer);
         return answer;
     }
 
-    let answer = (value * 2024, None);
-    compute_map.insert(value, answer);
-    answer
+    (value * 2024, None)
+    // compute_map.insert(*value, answer);
 }
 
 #[cfg(test)]
@@ -153,7 +158,7 @@ mod tests {
     #[test]
     fn test_process() -> Result<(), Error> {
         // let input = include_str!("input-example.txt");
-        let input = "400 30 121";
+        let input = "125 17";
         assert_eq!("55312", process(input)?);
         Ok(())
     }
